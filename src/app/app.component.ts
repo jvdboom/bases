d9601e5d54f8a0c65fe2cfc7aa887bcb1bf5c186
jvdboom <jvdboom@hotmail.com>
2020-01-06 11:25:00 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gr88-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gr88aa';
+}
